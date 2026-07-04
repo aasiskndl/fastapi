2a05cfaefcdb678b656946750d4a49f4c1859276
aasiskndl <aashishkandel90@gmail.com>
2026-07-04 20:24:06 +0545

    Added path parameter to the existing code, for the purpose of finding the patients using patients_id

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def load_data():
 
 @app.get("/")
 def Hello():
-    return {'message':'Hello'}
+    return {'message':'Hello this is the main page'}
 
 @app.get("/about")
 def about():
@@ -19,3 +19,16 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patients/{patient_id}")
+def view_patient(patient_id:int):
+    # load all the patents data
+    data = load_data()
+    patients = data['patients']
+    
+    for patient in patients:
+        if patient['id'] == patient_id:
+            return patient
+    return {'error':'Invalid patient ID'}
+
+    
