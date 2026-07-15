cf2328ce6df3698074bfead39cebc536ffb4b3e4
aasiskndl <aashishkandel90@gmail.com>
2026-07-15 12:23:20 +0545

    Used annotation for validation

--- pydantic_data_validation.py
+++ pydantic_data_validation.py
@@ -1,17 +1,26 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Annotated
 
 
 class Patient(BaseModel):
     
     # name must be within 50 characters
-    name: str = Field(max_length=50)
+    #name: str = Field(max_length=50)
+    
+    # we can use Annotated to add the metadata to the field
+    name: Annotated[str, Field(max_length=50, title='Name of patient', description="Give the name of the patient within 50 characters", examples=['hari', 'amit'])]
     
     # this checks that the values must be greater than 18 and less thatn 40 years
     age: int = Field(gt=18, lt=40)
     email: EmailStr
     url: AnyUrl
-    weight: float = Field(gt=0) # this is the constraint that will check if the value is >0 or not 
+    
+    #the weight will be inserted without any problem if it is passed as a string and it can cause problem if we need to do operation with this value , so to explicitly check if it is string or not we use Annotation 
+    #weight: float = Field(gt=0)  this is the constraint that will check if the value is >0 or not 
+    
+    weight: Annotated[float, Field(gt=0, strict=True)]
+    #(this strict = True checks if the value is of float or not) 
+    
     married: bool = False
     allergies: Optional[List[str]] = None 
     contact_details: Dict[str, str]
