f4c1765bddd740e6dd29f49e8abc71e3c7032f87
aasiskndl <aashishkandel90@gmail.com>
2026-07-15 11:50:58 +0545

    Performed data validation using pydantic

--- pydantic_data_validation.py
+++ pydantic_data_validation.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
 from typing import List, Dict, Optional
 
 
 class Patient(BaseModel):
-    name: str
-    age: int
+    
+    # name must be within 50 characters
+    name: str = Field(max_length=50)
+    
+    # this checks that the values must be greater than 18 and less thatn 40 years
+    age: int = Field(gt=18, lt=40)
     email: EmailStr
     url: AnyUrl
-    weight: float
+    weight: float = Field(gt=0) # this is the constraint that will check if the value is >0 or not 
     married: bool = False
     allergies: Optional[List[str]] = None 
     contact_details: Dict[str, str]
@@ -19,7 +23,7 @@ def insert_data(patient: Patient):
     
     print("inserted")
     
-patient_info = {'name':'Hari', 'age': 32, 'email':'hari@gmail.cpm', 'url':'https://aashish47.com.np', 'weight':89, 'married': False, 'allergies': ['pollen'], 'contact_details': {'phone':'9889239524'}}
+patient_info = {'name':'Hari', 'age': 22, 'email':'hari@gmail.cpm', 'url':'https://aashish47.com.np', 'weight':89, 'married': False, 'allergies': ['pollen'], 'contact_details': {'phone':'9889239524'}}
 patient1= Patient(**patient_info)
 
 insert_data(patient1)
