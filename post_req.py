6bff6720d27a755c14b4b8d63ba51989505fcc76
aasiskndl <aashishkandel90@gmail.com>
2026-07-27 22:35:55 +0545

    added put logic in the code

--- post_req.py
+++ post_req.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 
 import json
 
@@ -34,6 +34,14 @@ class Patient(BaseModel):
             return 'Normal'
         else:
             return 'Obese'
+        
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[str], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male','female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
         
         
 def load_data():
@@ -106,5 +114,15 @@ def create_patient(patient: Patient):
     
     
 
-
+#put logic
+@app.put('/edit/{patient_id}')
+def update_patients(patient_id: str, patient_update: PatientUpdate):
     
+    data = load_data()
+    
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient ID not found')
+    
+    existing_patient_info = data[patient_id]
+    
+    updated_patient_info= patient_update.model_dump(exclude_unset=True)
