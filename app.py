3a5f3cd124308878979099f54220f206cfb0487d
aasiskndl <aashishkandel90@gmail.com>
2026-08-08 18:25:20 +0545

    Model for prediction

---
+++ app.py
@@ -0,0 +1,110 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Literal, Annotated
+import pickle
+import pandas as pd
+
+# import the ml model
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+tier_1_cities = [
+    "Kathmandu",
+    "Lalitpur",
+    "Bhaktapur",
+]
+
+tier_2_cities = [
+    "Pokhara",
+    "Bharatpur",
+    "Biratnagar",
+    "Birgunj",
+    "Butwal",
+    "Dharan",
+    "Hetauda",
+    "Nepalgunj",
+    "Dhangadhi",
+    "Janakpur",
+    "Itahari",
+    "Birtamod",
+    "Mechinagar",
+    "Damak",
+    "Lahan",
+    "Rajbiraj",
+    "Ghorahi",
+    "Tulsipur",
+    "Siddharthanagar",
+    "Kirtipur",
+    "Madhyapur Thimi",
+    "Budhanilkantha",
+    "Tokha",
+    "Gokarneshwar",
+    "Chandragiri",
+    "Kageshwori Manohara",
+]
+
+#pydantic model to validate incomming data
+class UserInput(BaseModel):
+    
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient')]
+    income_lpa: Annotated[float, Field(..., gt=0, description="Salary of the user in LPA")]
+    smoker: Annotated[bool, Field(..., description="Does the user smokes or not?")]
+    city: Annotated[str, Field(..., description="The city where user lives in ")]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job', 'business_owner','unempoyed', 'private_job'], Field(..., description="Occupation of the user")]
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self) -> str:
+        if self.smoker and self.bmi > 30:
+            return "high" 
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"
+    
+    @computed_field
+    @property
+    def age_group(self) -> int:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "middle_aged"
+        return "senior"
+    
+    @computed_field
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        if self.city in tier_2_cities:
+            return 2
+        return 3
+    
+@app.post('/predict')
+def predict_premium(data: UserInput):
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+    
+    prediction = model.predict(input_df)[0]
+    
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+    
+        
